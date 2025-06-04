7a14943864067dbe2d028b50c2a68ea7a07c6721
Bartosz Ziarnik <bartosz.ziarnik@itsilesia.com>
2025-06-04 20:50:27 +0200

    feat: added swagger to prod

--- eCommerceApi/Program.cs
+++ eCommerceApi/Program.cs
@@ -24,12 +24,8 @@ builder.Services.AddScoped<IOrderService, OrderService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwaggerUI();
+app.UseSwagger();
 
 app.UseHttpsRedirection();
 
